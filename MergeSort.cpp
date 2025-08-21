f99ba876014c2eb443e3d7cf61153b14560df51c
Sucho6996 <sucharitsaha2002@gmail.com>
2025-08-21 15:47:40 +0530

    MergeSort added

---
+++ MergeSort.cpp
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+void swap(int arr[],int i,int j){
+	int temp=arr[i];
+	arr[i]=arr[j];
+	arr[j]=temp;
+}
+void merge(int arr[],int l,int mid,int r){
+	int i=l;
+	int j=mid;
+	int k=0;
+	int* mix = (int*) malloc((r - l) * sizeof(int));
+	while(i<mid && j<r){
+		if(arr[i]<arr[j]){
+			mix[k]=arr[i];
+			i++;
+		}
+		else{
+			mix[k]=arr[j];
+			j++;
+		}
+		k++;
+	}
+	while(i<mid){
+		mix[k]=arr[i];
+		i++;
+		k++;
+	}
+	if(j<r){
+		mix[k]=arr[j];
+		j++;
+		k++;
+	}
+	for(int x=0;x<(r-l);x++){
+		arr[l+x]=mix[x];
+	}
+	free(mix);
+}
+void ms(int arr[],int l,int r){
+	if(r-l<=1) return;
+	int mid=l+(r-l)/2;
+	ms(arr,l,mid);
+	ms(arr,mid,r);
+	merge(arr,l,mid,r);
+}
+
+
+int main(){
+	int arr[] = {5, 4, 3, 2, 1};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Before sorting:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    ms(arr,0,n);
+    printf("After sorting:\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+}
